bb82c0bfe2e1aae70bf5bc80ce687046d82ce5e8
AbhiJoy777 <joiceabhi00@gmail.com>
2026-06-12 11:51:38 +0530

    Connect user management page to backend API

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,27 +2,49 @@ const User = require("../models/User");
 const Task = require("../models/Task");
 
 const getAllUsers = async (req, res) => {
-  res.json({
-    message: "Get all users",
-  });
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getAllTasks = async (req, res) => {
-  res.json({
-    message: "Get all tasks",
-  });
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const deleteUser = async (req, res) => {
-  res.json({
-    message: "Delete user",
-  });
+  try {
+    await User.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const updateUserStatus = async (req, res) => {
-  res.json({
-    message: "Update user status",
-  });
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        status: req.body.status,
+      },
+      { new: true }
+    );
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
